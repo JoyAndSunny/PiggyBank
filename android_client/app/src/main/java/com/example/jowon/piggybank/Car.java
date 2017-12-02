903ca6d92fc21eb25323869d4b6d2f8787eada5d
Jowon Shin <joy.shin.2213@gmail.com>
2017-12-01 20:35:05 -0500

    created android project

---
+++ android_client/app/src/main/java/com/example/jowon/piggybank/Car.java
@@ -0,0 +1,19 @@
+package com.example.jowon.piggybank;
+
+/**
+ * Created by JOWON on 12/01/17.
+ */
+
+public class Car {
+    String name;
+    String year;
+    String model;
+    int wheelnum;
+
+    public Car(String name, String year, String model, int wheelnum) {
+        this.name = name;
+        this.year = year;
+        this.model = model;
+        this.wheelnum = wheelnum;
+    }
+}
